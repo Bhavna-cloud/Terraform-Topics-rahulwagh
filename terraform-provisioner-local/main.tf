302abdd803dd6ed378cdfd4f945faa8c643903a3
Bhavna <bhavna.rani199@gmail.com>
2025-01-08 15:11:52 +0530

    Update main.tf

--- terraform-provisioner-local/main.tf
+++ terraform-provisioner-local/main.tf
@@ -1,17 +1,18 @@
 provider "aws" {
-  region                   = "eu-central-1"
-  shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
+  region                   = "us-east-1"
+  shared_credentials_files = ["/home/bhavna/.aws/credentials"]
 }
 
 resource "aws_instance" "ec2_example" {
 
-    ami = "ami-0767046d1677be5a0"  
-    instance_type = "t2.micro" 
-    tags = {
+  ami                    = "ami-0e2c8caa4b6378d8c"
+  instance_type          = "t2.micro"
+  tags = {
         Name = "Terraform EC2"
     }
 
   provisioner "local-exec" {
-    command = "touch hello-jhooq.txt"
+    command = "touch hello.txt"
   }
 }
+  
