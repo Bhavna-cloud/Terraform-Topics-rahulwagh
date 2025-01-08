f35ad76bfb6b35e56d0f8c367ec279577ad7f183
Bhavna <bhavna.rani199@gmail.com>
2025-01-08 14:28:29 +0530

    Update main.tf

--- terraform-provisioner-file/main.tf
+++ terraform-provisioner-file/main.tf
@@ -1,32 +1,34 @@
 provider "aws" {
-  region                   = "eu-central-1"
-  shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
+  region                   = "us-east-1"
+  shared_credentials_files = ["/home/bhavna/.aws/credentials"]
 }
 
 resource "aws_instance" "ec2_example" {
 
-    ami = "ami-0767046d1677be5a0"  
-    instance_type = "t2.micro" 
-    key_name= "aws_key"
-    vpc_security_group_ids = [aws_security_group.main.id]
+  ami                    = "ami-0e2c8caa4b6378d8c"
+  instance_type          = "t2.micro"
+  key_name               = "id_rsa"
+  vpc_security_group_ids = [aws_security_group.main.id]
+
 
   provisioner "file" {
-    source      = "/home/rahul/Jhooq/keys/aws/test-file.txt"
+    source      = "/home/bhavna/practice/terraform-state-locking/test-file.txt"
     destination = "/home/ubuntu/test-file.txt"
   }
   connection {
-		type        = "ssh"
-		host        = self.public_ip
-		user        = "ubuntu"
-		private_key = file("/home/rahul/Jhooq/keys/aws/aws_key")
-		timeout     = "4m"
-	}
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("/home/bhavna/practice/ssh/id_rsa")
+    timeout     = "4m"
+  }
 }
+
 
 resource "aws_security_group" "main" {
   egress = [
     {
-      cidr_blocks      = [ "0.0.0.0/0", ]
+      cidr_blocks      = ["0.0.0.0/0", ]
       description      = ""
       from_port        = 0
       ipv6_cidr_blocks = []
@@ -37,23 +39,23 @@ resource "aws_security_group" "main" {
       to_port          = 0
     }
   ]
- ingress                = [
-   {
-     cidr_blocks      = [ "0.0.0.0/0", ]
-     description      = ""
-     from_port        = 22
-     ipv6_cidr_blocks = []
-     prefix_list_ids  = []
-     protocol         = "tcp"
-     security_groups  = []
-     self             = false
-     to_port          = 22
-  }
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
   ]
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "aws_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbvRN/gvQBhFe+dE8p3Q865T/xTKgjqTjj56p1IIKbq8SDyOybE8ia0rMPcBLAKds+wjePIYpTtRxT9UsUbZJTgF+SGSG2dC6+ohCQpi6F3xM7ryL9fy3BNCT5aPrwbR862jcOIfv7R1xVfH8OS0WZa8DpVy5kTeutsuH5FMAmEgba4KhYLTzIdhM7UKJvNoUMRBaxAqIAThqH9Vt/iR1WpXgazoPw6dyPssa7ye6tUPRipmPTZukfpxcPlsqytXWlXm7R89xAY9OXkdPPVsrQA0XFQnY8aFb9XaZP8cm7EOVRdxMsA1DyWMVZOTjhBwCHfEIGoePAS3jFMqQjGWQd rahul@rahul-HP-ZBook-15-G2"
-}
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "id_rsa"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDeJpsit8MACLsgDZVW+EkuMIqyAQh2c/MrhAf07FbnhKNcGRwTS8/75+mrimgYWYB20octWA/unOya4ffzy2Tp0xP7BgwesMppIdpcxtK4EwYiIKGBlubeLt+KA9jQEOgxWYbffnNqaKGIKn7D55Dlbz75iP4S3FUP63/PyJSi50V/mMYa89J1b+X/Y65ssqi3mFvHZI5Yq8LvsA+v4xkam6T/q6eXD212tRfv54LqoxoMG4Xdf4UcrfwGajGMK4lbeJYyvbLT5qRxzT6qjr0zkmuXlG5P7Q9Mtb0xCOD/mpZstG4I4AEWOXVqPbBSL729wcxLtVACRFJg/01cNk6f bhavna@server.bhavna.local"
+}
