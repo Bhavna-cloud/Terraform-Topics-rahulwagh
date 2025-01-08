de229f3e213bfd0a1ed4adee061097a011402f66
Bhavna <bhavna.rani199@gmail.com>
2025-01-08 13:19:29 +0530

    Update main.tf

--- terraform-state-locking/project-2/main.tf
+++ terraform-state-locking/project-2/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-   region     = "eu-central-1"
+   region     = "us-east-1"
    access_key = var.access_key
    secret_key = var.secret_key
 }
 
 resource "aws_instance" "ec2_example" {
-    ami = "ami-0767046d1677be5a0"
+    ami = "ami-0e2c8caa4b6378d8c"
     instance_type = "t2.micro"
     tags = {
       Name = "EC2 Instance with remote state - developer 2"
@@ -14,10 +14,10 @@ resource "aws_instance" "ec2_example" {
 
 terraform {
   backend "s3" {
-    bucket         = "jhooq-terraform-s3-bucket"
-    key            = "jhooq/terraform/remote/s3/terraform.tfstate"
+    bucket         = "bhavna-terraform-s3-bucket"
+    key            = "abc/terraform.tfstate"
     encrypt        = true
-    region         = "eu-central-1"
+    region         = "us-east-1"
     dynamodb_table = "dynamodb-state-locking"
   }
 }
